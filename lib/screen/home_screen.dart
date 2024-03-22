2c5a58b02d8c45bcd28677baade0bfcec7d5e7eb
cmj <chmin82@gmail.com>
2024-03-22 20:43:58 +0900

    코드팩토리의 플러터 프로그래밍 CH08

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController webViewController = WebViewController()
+    ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
+    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+      ),
+      body: WebViewWidget(
+        controller: webViewController,
+      ),
+    );
+  }
+}
